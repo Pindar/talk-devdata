17be8d129dad346219409af352b6af305012d43b
Simon Dittlmann <simon.dittlmann@sinnerschrader.com>
2017-04-30 12:06:46 +0200

    implement random CF

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # talk-devdata
+
+# Deployment
+
+gcloud beta functions deploy testApi1 --source-url https://source.developers.google.com/p/sd-talk-devdata/r/talk-devdata --source-path /test-api-1 --trigger-http
---
+++ test-api-1/index.js
@@ -0,0 +1,35 @@
+
+
+function handleGET (req, res) {
+  setTimeout(function () {
+      res.json({ message: 'hooray! welcome to our api!' });
+  }, Math.random() * 3000);
+}
+
+function handlePUT (req, res) {
+  // Do something with the PUT request
+  res.status(403).send('Forbidden!');
+}
+
+/**
+ * Responds to a GET request with "Hello World!". Forbids a PUT request.
+ *
+ * @example
+ * gcloud beta functions call testApi1
+ *
+ * @param {Object} req Cloud Function request context.
+ * @param {Object} res Cloud Function response context.
+ */
+exports.testApi1 = function helloHttp (req, res) {
+  switch (req.method) {
+    case 'GET':
+      handleGET(req, res);
+      break;
+    case 'PUT':
+      handlePUT(req, res);
+      break;
+    default:
+      res.status(500).send({ error: 'Something blew up!' });
+      break;
+  }
+};
